80e8becc8d51b7f9f011916031917a385b793a15
NepherDios <rui.moreira.programmer@gmail.com>
2025-10-01 22:24:21 +0100

    Updated Ability Model and Schema

--- models/ability.py
+++ models/ability.py
@@ -7,8 +7,12 @@ class Ability(Base):
     
     ability_id = Column(Integer, primary_key=True, index=True)
     player_id = Column(Integer, ForeignKey("Player.player_id"), nullable=False, index=True)
-    ability_key = Column(String(20), unique=True)
+    ability_name = Column(String(30))
+    ability_key = Column(String(20))
     ability_slot = Column(Integer)
     
     player = relationship('Player', back_populates='ability')
-    __table_args__ = (UniqueConstraint('player_id', 'ability_key', name='uq_player_ability_key'),)
+    __table_args__ = (
+        UniqueConstraint('player_id', 'ability_key', name='uq_player_ability_key'),
+        UniqueConstraint('player_id', 'ability_name', name='uq_player_ability_name'),
+    ) 
--- schemas/ability.py
+++ schemas/ability.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 
 class AbilityCreate(BaseModel):
     player_id: int
+    ability_name: str
     ability_key: Optional[str] = None
     ability_slot: Optional[int] = None
 
 
 class AbilityUpdate(BaseModel):
+    ability_name: Optional[str]
     ability_key: Optional[str]
     ability_slot: Optional[int]
 
@@ -15,6 +17,7 @@ class AbilityUpdate(BaseModel):
 class AbilityResponse(BaseModel):
     ability_id: int
     player_id: int
+    ability_name: str
     ability_key: Optional[str] = None
     ability_slot: Optional[int] = None
 
