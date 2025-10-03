6e41864f95b41b1d75a080dd91dee9f51b2d6b3c
NepherDios <rui.moreira.programmer@gmail.com>
2025-10-03 13:15:46 +0100

    a

--- crud/save.py
+++ crud/save.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
-from models.save import Save
-from schemas.saves import CreateSave, SaveResponse, UpdateSave
+from models.save import PlayerSave
+from schemas.saves import CreateSave, UpdateSave
 
-def create_save(session: Session, data: CreateSave) -> Save | None:
-    save = Save(**data.dict())
+def create_save(session: Session, data: CreateSave) -> PlayerSave | None:
+    save = PlayerSave(**data.dict())
     
     session.add(save)
     session.commit()
@@ -12,12 +12,12 @@ def create_save(session: Session, data: CreateSave) -> Save | None:
     return save
 
 
-def get_saves(session: Session, player_id: int) -> list[Save] | None:
-    return session.query(Save).filter(Save.player_id == player_id).all()
+def get_saves(session: Session, player_id: int) -> list[PlayerSave] | None:
+    return session.query(PlayerSave).filter(PlayerSave.player_id == player_id).all()
     
     
-def update_saves(session: Session, player_id: int, save_slot: int, updated_data: UpdateSave) -> Save | None:
-    save = session.query(Save).filter(Save.player_id == player_id, Save.save_slot == save_slot).first()
+def update_saves(session: Session, player_id: int, save_slot: int, updated_data: UpdateSave) -> PlayerSave | None:
+    save = session.query(PlayerSave).filter(PlayerSave.player_id == player_id, PlayerSave.save_slot == save_slot).first()
     
     if not save:
         return None
@@ -30,8 +30,8 @@ def update_saves(session: Session, player_id: int, save_slot: int, updated_data:
     
     return save
     
-def delete_save(session: Session, player_id: int, save_slot: int) -> Save | None:
-    save = session.query(Save).filter(Save.player_id == player_id, Save.save_slot == save_slot).first()
+def delete_save(session: Session, player_id: int, save_slot: int) -> PlayerSave | None:
+    save = session.query(PlayerSave).filter(PlayerSave.player_id == player_id, PlayerSave.save_slot == save_slot).first()
     
     if save:
         session.delete(save)
