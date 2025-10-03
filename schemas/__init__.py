ccba4039b9cd072c1213f6f6d7cdd16909f17074
NepherDios <rui.moreira.programmer@gmail.com>
2025-10-03 12:11:36 +0100

    Update

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,14 +1,14 @@
-from .player import PlayerCreate, PlayerUpdate, PlayerResponse
+from .player import PlayerCreate, PlayerUpdate, PlayerResponse, PlayersListResponse
 from .ability import AbilityCreate, AbilityUpdate, AbilityResponse
-from .inventory import InventoryCreate, InventoryUpdate, InventoryResponse
-from .item import ItemCreate, ItemResponse
+from .inventory import *
+from .item import ItemCreate, ItemResponse, ItemUpdate, ItemDetailsWithCommonAttr
 from .equipment_common_attr import EquipmentCommonAttrCreate, EquipmentCommonAttrUpdate, EquipmentCommonAttrResponse
-from .general_item import GeneralItemCreate, GeneralItemResponse
+from .general_item import GeneralItemCreate, GeneralItemResponse, GeneralItemUpdate
 from .weapon import WeaponCreate, WeaponUpdate, WeaponResponse, WeaponType
 from .armor import ArmorCreate, ArmorUpdate, ArmorResponse, ArmorType
 from .accessory import AccessoryCreate, AccessoryResponse
 from .roll_stat import RollStatCreate, RollStatResponse
 from .armory_enchantment import ArmoryEnchantmentCreate, ArmoryEnchantmentUpdate, ArmoryEnchantmentResponse
 from .progress import ProgressCreate, ProgressUpdate, ProgressResponse
-from .quest import QuestCreate, QuestUpdate, QuestResponse
-from .quest_progress import QuestProgressCreate, QuestProgressUpdate, QuestProgressResponse
+from .quest import QuestCreate, QuestUpdate, QuestResponse, QuestListResponse
+from .quest_progress import QuestProgressCreate, QuestProgressUpdate, QuestProgressResponse, QuestProgressListResponse
