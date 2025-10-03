7940e0795412968ab32f0c37f46f781d561e735f
NepherDios <rui.moreira.programmer@gmail.com>
2025-10-03 12:52:49 +0100

    Update

--- models/skill_tree.py
+++ models/skill_tree.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
 from database import Base
@@ -8,6 +9,6 @@ class SkillTree(Base):
     
     skill_tree_id = Column(Integer, primary_key=True, index=True)
     player_id = Column(Integer, ForeignKey("Player.player_id"), index=True)
-    skills = Column(ARRAY(Integer), default=list)
+    skills = Column(MutableList.as_mutable(ARRAY(Integer)), default=[])
     
     player = relationship('Player', back_populates='skill_tree')
